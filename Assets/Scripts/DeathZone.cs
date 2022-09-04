3efb448c6200579c8995a95bcc76ec5ddc289395
NG-user1458 <Wassim.Games2021@outlook.fr>
2022-09-04 15:20:39 +0200

    Changement mineur death zone

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -2,13 +2,11 @@ using UnityEngine;
 
 public class DeathZone : MonoBehaviour
 {
-    public Transform CheckPoint;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player")) 
         {
-            collision.gameObject.transform.position = CheckPoint.position;
+            PlayerHealth.instance.TakeDamage(100);
         }
     }
 }
